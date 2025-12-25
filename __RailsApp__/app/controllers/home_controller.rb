0b4dbfde3c388c8c876336c19097ea2720f40e1f
Ilya N. Zykin <zykin-ilya@ya.ru>
2025-12-25 23:26:22 +0300

    Update Index Page

--- __RailsApp__/app/controllers/home_controller.rb
+++ __RailsApp__/app/controllers/home_controller.rb
@@ -5,5 +5,6 @@ class HomeController < ApplicationController
     @ruby_version = RUBY_VERSION
     @ruby_description = RUBY_DESCRIPTION
     @yjit_enabled = defined?(RubyVM::YJIT) && RubyVM::YJIT.enabled?
+    @zjit_enabled = defined?(RubyVM::ZJIT) && RubyVM::ZJIT.enabled?
   end
 end
