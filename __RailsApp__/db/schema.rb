3c4393ebf2cd8e5a25bb97f609469b2af00c388f
Ilya N. Zykin <zykin-ilya@ya.ru>
2026-01-02 15:09:14 +0300

    Posts Initial Setup

--- __RailsApp__/db/schema.rb
+++ __RailsApp__/db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_02_120830) do
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.boolean "published"
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
 end
